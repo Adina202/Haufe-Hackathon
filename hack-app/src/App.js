c6c20cf3f25985750775d2831b2fe0409c2ebc2b
Adina202 <102593788+Adina202@users.noreply.github.com>
2022-06-04 15:37:13 +0300

    add initial implementation

--- hack-app/src/App.js
+++ hack-app/src/App.js
@@ -1,16 +1,61 @@
-import React from 'react';
-// import Map from './components/Map';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 // TODO: start here
 
 function App() {
-  return (
-    <>
-      <h1>Haufe Hackathon</h1>
-      {/* <Map/> */}
-    </>
-  );
+
+    
+    const [data, setData] = useState([]);
+
+    // GET request function to your Mock API
+    const fetchData = () => {
+        axios
+            .get('http://localhost:4000/data')
+            .then(resp => {
+                console.log(`statusCode: ${resp.status}`);
+                //console.log(resp.data.records);
+                setData(resp.data.records);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    // Calling the function on component mount
+    useEffect(() => {
+        fetchData();
+    }, []);
+    console.log(data);
+    return (
+        <>
+
+            <h1>Haufe Hackathon</h1>
+
+            <table id="simple-board">
+                <tbody>
+                    { 
+                      data.map((item) => (
+                          <tr key={item.id}>
+                              <td>{item._id}</td>
+                              <td>{item.id}</td>
+                              <td>{item.tipcolectare}</td>
+                              <td>{item.adresa}</td>
+                              <td>{item.companie}</td>
+                              <td>{item.website}</td>
+                              <td>{item.latitudine}</td>
+                              <td>{item.longitudine}</td>
+                              <td>{item.count}</td>
+                              <td />
+                          </tr>
+                      ))
+                  }
+                </tbody>
+            </table>
+            {/* <Map/> */}
+        </>
+    );
+
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,48 @@
 // TODO: start here
+const axios = require('axios');
+/*
+axios
+    .get('https://data.primariatm.ro/api/3/action/datastore_search?resource_id=d0134630-84d9-40b8-9bcb-dfdc926d66ab&limit=5')
+    .then(res => {
+        console.log(`statusCode: ${res.status}`);
+        console.log(res.data.result);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+    */
+
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+const cors = require('cors');
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('smecher')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/cool', (req, res) => {
+    res.send('si mai smecher')
+})
+
+app.get('/data', (req, res) => {
+    axios
+        .get('https://data.primariatm.ro/api/3/action/datastore_search?resource_id=d0134630-84d9-40b8-9bcb-dfdc926d66ab&limit=5')
+        .then(resp => {
+            console.log(`statusCode: ${resp.status}`);
+            console.log(resp.data.result);
+            res.send(resp.data.result);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    
+})
